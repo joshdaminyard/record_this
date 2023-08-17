6093cc5fac842995edba736e9ba26ce325c6bf0b
Josh Minyard <joshdaminyard@gmail.com>
2023-08-16 19:55:02 -0700

    added boilerplate for album class

--- lib/classes/Album.dart
+++ lib/classes/Album.dart
@@ -1,1 +1,22 @@
+// class Album {
+//   final int userId;
+//   final int id;
+//   final String title;
+//   final String artist;
 
+//   const Album({
+//     required this.userId,
+//     required this.id,
+//     required this.title,
+//     required this.artist,
+//   });
+
+//   factory Album.fromJson(Map<String, dynamic> json) {
+//     return Album(
+//       userId: json['userId'],
+//       id: json['id'],
+//       title: json['title'],
+//       artist: json['artist'],
+//     );
+//   }
+// }
