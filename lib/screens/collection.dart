7ae8bff36a9b6578bb75ef1320dbcc1f99bf42b5
Josh Minyard <joshdaminyard@gmail.com>
2023-08-19 17:03:42 -0700

    add seperate functionalit when query returns nothing

--- lib/screens/collection.dart
+++ lib/screens/collection.dart
@@ -11,7 +11,10 @@ class CollectionPage extends StatelessWidget {
 
     DatabaseEvent event = await ref.once();
 
-    //shows the data here
+    // check if query returned with nothing
+    if (!event.snapshot.exists) {
+      return [];
+    }
     return event.snapshot.value;
   }
 
@@ -26,9 +29,26 @@ class CollectionPage extends StatelessWidget {
       body: FutureBuilder(
           future: databaseQuery(),
           builder: (context, snapshot) {
+            // data is loaded from query
             if (snapshot.hasData) {
               var data = snapshot.data as List<dynamic>;
-              debugPrint("theres data!");
+
+              //query returns with nothing
+              if (data.isEmpty) {
+                return Center(
+                    child: Scrollbar(
+                        controller: scrollController,
+                        thumbVisibility: true,
+                        child: SingleChildScrollView(
+                          controller: scrollController,
+                          child: const Column(children: [
+                            Text("There is no data"),
+                            SizedBox(height: 10),
+                          ]),
+                        )));
+              }
+
+              // query has data so show albums
               return Center(
                   child: Scrollbar(
                       controller: scrollController,
@@ -40,7 +60,10 @@ class CollectionPage extends StatelessWidget {
                           const SizedBox(height: 10),
                         ]),
                       )));
-            } else if (snapshot.hasError) {
+            }
+
+            // when query returns with an error
+            else if (snapshot.hasError) {
               return Center(
                   child: Scrollbar(
                       controller: scrollController,
@@ -53,22 +76,8 @@ class CollectionPage extends StatelessWidget {
                         ]),
                       )));
             }
-            /*
-                have proper way to check if there is no data
-            */
-            // else if (!snapshot.hasData) {
-            //   return Center(
-            //       child: Scrollbar(
-            //           controller: scrollController,
-            //           thumbVisibility: true,
-            //           child: SingleChildScrollView(
-            //             controller: scrollController,
-            //             child: const Column(children: [
-            //               Text("No Data"),
-            //               SizedBox(height: 10),
-            //             ]),
-            //           )));
-            // }
+
+            // show loading symbol while data is still being loaded
             else {
               return const Center(
                 child: CircularProgressIndicator(),
@@ -76,10 +85,5 @@ class CollectionPage extends StatelessWidget {
             }
           }),
     );
-
-    // return const Padding(
-    //   padding: EdgeInsets.all(10.0),
-    //   child: Text("Collection Page"),
-    // );
   }
 }
