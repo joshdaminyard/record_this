5e1f34acb1a3da37f6fd5db63b9368fcc2a6d301
Josh Minyard <joshdaminyard@gmail.com>
2023-08-25 23:17:19 -0700

    created initial search page

---
+++ lib/screens/search.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({super.key});
+
+  Future<Object?> discogQuery() async {
+    DatabaseReference ref = FirebaseDatabase.instance.ref("collection/albums");
+
+    DatabaseEvent event = await ref.once();
+
+    // check if query returned with nothing
+    if (!event.snapshot.exists) {
+      return [];
+    }
+    return event.snapshot.value;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final ScrollController scrollController = ScrollController();
+    final formKey = GlobalKey<FormState>();
+
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Album Collection'),
+        ),
+        body: const Center(
+          child: Column(children: [
+            Text("Title"),
+            Form(child: TextField()),
+            Text("Artist"),
+            Form(child: TextField())
+          ]),
+        ));
+  }
+}
