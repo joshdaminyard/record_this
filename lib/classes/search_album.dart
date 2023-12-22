79c39fb0fb18f1797f8b844dfa7d9eec0bd57250
Josh Minyard <joshdaminyard@gmail.com>
2023-12-22 13:19:36 -0800

    add search album class for passing album info

---
+++ lib/classes/search_album.dart
@@ -0,0 +1,9 @@
+class SearchAlbum {
+  final String title;
+
+  final String artist;
+  final String label;
+  final String year;
+
+  const SearchAlbum(this.title, this.artist, this.label, this.year);
+}
