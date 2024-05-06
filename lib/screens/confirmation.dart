f6e1c24caa91cfbaea916e4f574fd4e7a8a3a0db
Josh Minyard <joshdaminyard@gmail.com>
2024-05-06 13:02:35 -0700

    added adding functionality

--- lib/screens/confirmation.dart
+++ lib/screens/confirmation.dart
@@ -16,6 +16,21 @@ class ConfirmationPage extends StatelessWidget {
     FirebaseDatabase.instance.ref("collection/albums/$albumID").remove();
   }
 
+  Future<bool> addAlbum(dynamic album, String albumID) async {
+    DatabaseReference ref =
+        FirebaseDatabase.instance.ref("collection/albums/$albumID");
+
+    DatabaseEvent event = await ref.once();
+
+    if (!event.snapshot.exists) {
+      await ref.set(album);
+      return true;
+    }
+
+    //return false if the album was not added since its already in the db
+    return false;
+  }
+
   @override
   Widget build(BuildContext context) {
     final title = album["title"].toString();
@@ -42,29 +57,81 @@ class ConfirmationPage extends StatelessWidget {
                       onPressed: () async {
                         if (confType == "Delete") {
                           removeAlbum();
-                          Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      const CollectionPage()));
-                        } else {
-                          /*
-                            TODO:
-                            
-                            1. Check if album is already in the collection
-                            2a. if it is pop up and say already in it and have a
-                                button that goes back to add page
-                            2b. if not add to page and go back to colleciton page
-                          */
 
-                          Navigator.pushReplacement(
+                          Navigator.pushAndRemoveUntil(
                               context,
                               MaterialPageRoute(
-                                  builder: (context) =>
-                                      const CollectionPage()));
+                                  builder: (context) => const CollectionPage()),
+                              (route) => false);
+                        }
+                        //Adding an album
+                        else {
+                          bool status = await addAlbum(album, albumID);
+
+                          //The album is already in the collection so show a popup
+                          //and go back to the add screen
+                          if (!status) {
+                            if (context.mounted) {
+                              await _showMyDialog(context);
+                            }
+                          }
+                          //The album isnt in the collection so go to collection
+                          //screen after it was added
+                          else {
+                            if (context.mounted) {
+                              Navigator.pushAndRemoveUntil(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const CollectionPage()),
+                                  (route) => false);
+                            }
+                          }
                         }
                       },
                       child: const Text("Confirm"))
                 ]))));
   }
+}
+
+//show popup when album is already in the db with 2 options going
+//back to the collection page or the add album list page
+Future<void> _showMyDialog(BuildContext context) async {
+  showDialog<void>(
+    context: context,
+    barrierDismissible: false,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        actionsAlignment: MainAxisAlignment.center,
+        title: const Text('Album in Collecion'),
+        content: const SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              Text('This album is already in your collection'),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          TextButton(
+            child: const Text('Go Back'),
+            onPressed: () {
+              Navigator.of(context).pop();
+              Navigator.of(context).pop();
+              Navigator.of(context).pop();
+            },
+          ),
+          TextButton(
+            child: const Text('Go to Collection'),
+            onPressed: () {
+              Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const CollectionPage()),
+                  (route) => false);
+            },
+          ),
+        ],
+      );
+    },
+  );
 }
